318f5d707096038f6bf9a055b1b48058c9ed7c47
Bradley Haskell <156127322+bradhaskell@users.noreply.github.com>
2025-05-06 06:33:48 +0000

    ELT process for Quiz 3

---
+++ elt/basket_craft_website_sessions_extract_load_raw.py
@@ -0,0 +1,56 @@
+"""
+1. Import necessary libraries
+2. Load MYSQL and Postgres connection details
+3. Build connection strings and create database engines
+4. Read products table from MySQL and load into a DataFrame
+5. Write DataFrame to products table in Postgres (raw schema)
+"""
+#%%
+# Import necessary libraries
+import pandas as pd
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+
+#%%
+# Load environment variables from .env file
+load_dotenv()
+
+#%%
+# Load MYSQL and Postgres connection details
+mysql_user = os.environ['MYSQL_USER']
+mysql_password = os.environ['MYSQL_PASSWORD']
+mysql_host = os.environ['MYSQL_HOST']
+mysql_db = os.environ['MYSQL_DB']
+
+# Postgres database connection details
+pg_user = os.environ['PG_USER']
+pg_password = os.environ['PG_PASSWORD']
+pg_host = os.environ['PG_HOST']
+pg_db = os.environ['PG_DB']
+
+#%%
+# Build connection strings and create database engines
+mysql_conn_str = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}'
+pg_conn_str = f'postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}/{pg_db}'
+
+# %%
+# Create database engines
+mysql_engine = create_engine(mysql_conn_str)
+pg_engine = create_engine(pg_conn_str)
+
+# %%
+# Display the first few rows of the DataFrame
+df = pd.read_sql('SELECT * FROM website_sessions', mysql_engine)
+
+# %%
+df
+
+# %%
+# Write DataFrame to products table in Postgres (raw schema)
+with pg_engine.begin() as conn:
+    conn.execute(text("CREATE SCHEMA IF NOT EXISTS raw"))
+df.to_sql('website_sessions', pg_engine, schema='raw', if_exists='append', index=False)
+
+# %%
+print(f'{len(df)} records loaded into Postgres raw.website_sessions table.')
